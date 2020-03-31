61cd3fe535041e0cb983dceed57ecfa091a44bcb
Luke Donovan <lukelamnisos@icloud.com>
2020-03-31 16:05:11 +0100

    cleaned up repo

--- spec/test-note-model.js
+++ spec/test-note-model.js
@@ -3,9 +3,6 @@
     var note = new Note("hello");
 
     assert.isTrue(note.text === "hello");
-    // if (note.text !== "hello") {
-    //   throw new Error("Expected note.text to return 'hello'")
-    // }
   }
   testNotesReturnsText();
 })(this);
